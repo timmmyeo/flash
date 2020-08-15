e5272b1b8375e975c0b05af739baf77788280c0c
Timothy Yeo <tgy19@ic.ac.uk>
2020-08-16 00:56:57 +0100

    Made a TODO, added a stepper to step through all the cards on the main url

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 It's just a small project I'm using to learn material-ui and React, don't blame me if it doesn't work properly ;)
+https://timmmmyeo.github.io/ - the main page
+https://timmmmyeo.github.io/edit - the editor for cards
+
+TODO:
+- Add button to reveal the content of the card
+- After revealed, ask user "correct? "Not correct?" and keep a score to show on the final page 
+- (toast: "nice!" if they get it right, "better luck next time!" otherwise)
+- Create final page, and don't allow users to switch back to a previous flashcard (maybe make this an option later)
+- Make content of the card bigger to fill the screen
+- Add a menu to switch between the editor and using the flashcards
--- src/PlayFlash.js
+++ src/PlayFlash.js
@@ -4,9 +4,16 @@ import MobileStepper from '@material-ui/core/MobileStepper';
 import Button from '@material-ui/core/Button';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid'
 
 export default function PlayFlash(props) {
   const [i, setI] = React.useState(3);
+  const [revealed, setRevealed] = React.useState(false);
 
   const flashcard = props.flashcards[i];
   
@@ -20,11 +27,50 @@ export default function PlayFlash(props) {
     setI((i) => i - 1);
   };
 
+  const useStyles = makeStyles({
+    root: {
+      minWidth: 275,
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+
+  const classes = useStyles();
+
   return (
     <>
-      <h1>{flashcard.title}</h1>
-      <h2>{flashcard.subtitle}</h2>
-      <h3>{flashcard.content}</h3>
+    <Container maxWidth="lg">
+    {/* Can I abstract this out by using the FlashCard Component? */}
+    <Card className={classes.root}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            {flashcard.title}
+          </Typography>
+          <br />
+          <Typography className={classes.pos} color="textSecondary">
+            {flashcard.subtitle}
+          </Typography>
+          {revealed && 
+          <Typography variant="body2" component="p">
+            {flashcard.content}
+          </Typography>
+          }       
+        </CardContent>
+        <CardActions>
+          <Grid container>
+            <Grid item xs={2} md={2} align="center">
+            </Grid>
+            <Grid item xs={8} md={8}/>
+            <Grid item xs={2} md={2} align="center">
+            </Grid>
+          </Grid>
+        </CardActions>
+      </Card>
+    </Container>
       
 
       <MobileStepper
