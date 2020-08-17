dac4f6d78ee2aae66e8ae2ee415c3a616788a403
Timothy Yeo <tgy19@ic.ac.uk>
2020-08-17 21:41:12 +0100

    Tidied up example cards, updated TODO

--- src/App.js
+++ src/App.js
@@ -15,94 +15,9 @@ function App() {
   const [flashcards, setFlashcards] = React.useState([
     { 
       id: 1,
-      title: "title", 
-      subtitle: "subtitle1",
-      content: "this is some text"
-    },
-    {
-      id: 2,
-      title: "title2", 
-      subtitle: "subtitle2",
-      content: "this is some more text"
-    },
-    {
-      id: 3,
-      title: "title3", 
-      subtitle: "3",
-      content: "no3"
-    },
-    {
-      id: 4,
-      title: "yay", 
-      subtitle: "more4",
-      content: "this is  more text4"
-    },
-    { 
-      id: 5,
-      title: "5thing", 
-      subtitle: "subtitle5",
-      content: "this is some more tex5"
-    },
-    {
-      id: 6,
-      title: "6a", 
-      subtitle: "subtitle6",
-      content: "this is some more tex6"
-    },
-    {
-      id: 7,
-      title: "6a", 
-      subtitle: "subtitle6",
-      content: "this is some more tex6"
-    },
-    {
-      id: 8,
-      title: "6a", 
-      subtitle: "subtitle6",
-      content: "this is some more tex6"
-    },
-    {
-      id: 9,
-      title: "6a", 
-      subtitle: "subtitle6",
-      content: "this is some more tex6"
-    },
-    {
-      id: 10,
-      title: "6a", 
-      subtitle: "subtitle6",
-      content: "this is some more tex6"
-    },
-    {
-      id: 11,
-      title: "6a", 
-      subtitle: "subtitle6",
-      content: "this is some more tex6"
-    },
-    {
-      id: 12,
-      title: "6a", 
-      subtitle: "subtitle6",
-      content: "this is some more tex6"
-    },
-    {
-      id: 13,
-      title: "6a", 
-      subtitle: "subtitle6",
-      content: "this is some more tex6"
-    },
-    {
-      id: 14,
-      title: "6a", 
-      subtitle: "subtitle6",
-      content: "this is some more tex6"
-    },
-    {
-      id: 15,
-      title: "6a", 
-      subtitle: "subtitle6",
-      content: "this is some more tex6"
-    },
+      title: "This is an example title", 
+      subtitle: "This is an example subtitle",
+      content: "This is some text"
   ]);
 
   
